439f01efd5f4b77c31fa52d2a10a7b79886a7024
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-12-03 20:58:59 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
